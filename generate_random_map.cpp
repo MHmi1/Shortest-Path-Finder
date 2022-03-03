bc8247e3b7590cd1757e1ef0f1ad5dc993f436de
Hami <78771728+MHmi1@users.noreply.github.com>
2022-03-03 06:04:04 +0330

    Update generate_random_map.cpp
    fixing bugs and ...
    :)

--- generate_random_map.cpp
+++ generate_random_map.cpp
@@ -1,53 +1,62 @@
 #include <fstream>
 #include <iomanip>
-#include <cmath>
-#include <string>
 #include <iostream>
 #include <algorithm>
 #include <random>
 #include <time.h>
 #include <conio.h>
 
-#define row_size 30 // Number of matrix rows
-#define col_size 30 //Number of matrix columns 
+#define row_size 25		// Number of matrix rows
+#define col_size 25		//Number of matrix columns
 
 
 using namespace std;
 
-int main()
+int
+main ()
 {
-    FILE *f;
-    fstream out;
+  FILE *f;
+  fstream out;
 
-    out.open("map2.txt", ios::out | ios::ate | ios::in); //opening file to write data
-    if (!out.is_open())
+  out.open ("map2.txt", ios::out | ios::ate);	//opening file to write data
+  if (!out.is_open ())
     {
-        cerr << "error in opening data file !!" << endl;
+      cerr << " error in opening data file !!" << endl;
     }
 
-srand(time(0));
-    random_device rd;
-    default_random_engine dre(rd());
+  srand (time (0));
+  random_device rd;
+  default_random_engine dre (rd ());
 
-   for (int k=0;k<col_size;k++)
-   {
-    uniform_int_distribution<int> uid(1,17); // generate random num 1 - 17 
-     int arr[row_size];
-    generate(arr, arr + sizeof(arr) / sizeof(int), [&] () { return uid(dre); });
+  for (int k = 0; k < col_size; k++)
+    {
+      uniform_int_distribution < int >uid (1, 17);	// generate random num between 1 - 17 
 
-  for (int i=0;i<row_size;i++)
-  {
+      int arr[row_size];
+
+      generate (arr, arr + sizeof (arr) / sizeof (int),[&]()  //lambda expression
+		{
+		return uid (dre);
+		}
+      );
+
+      for (int z = 0; z < row_size; z++)	//map should not have 4 because destination input from the user
+	{
+	  if (arr[z] == 4)
+	    arr[z] = 5;
+	}
+
+      for (int i = 0; i < row_size; i++)
+	{
+	  out << arr[i] << " ";
+	}
+      out << endl;
+    }
 
 
-out <<arr[i]<<" ";
+  out.flush ();
+  out.close ();
+  getch ();
+  return 0;
 
-  }
-  out<<endl;
-   }
-
-   out.flush();
-   out.close();
-   getch();
-    return 0;
-	
 }
